ec25bde3c133bdee06094fb4bf882337905cbe03
Oscar Shaw <oscar.shaw@yahoo.com>
2018-07-14 12:06:48 -0700

    app.js: Reconfigure port

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 /*
  * Module dependencies
  */
-var express = require('express')
-  , stylus = require('stylus')
-  , nib = require('nib')
+const express = require('express')
+const stylus = require('stylus')
+const nib = require('nib')
 
-var app = express();
+const app = express();
 
 function compile(str, path) {
   return stylus(str)
     .set('filename', path)
     .use(nib());
 }
-var firebase = require("firebase");
+const firebase = require("firebase");
 
-var config = {
+const config = {
   apiKey: "AIzaSyBuEmpNHMIzsEE64P2MTYZzDyeOYJ3HVe4",
   authDomain: "climatefox-b9ab1.firebaseapp.com",
   databaseURL: "https://climatefox-b9ab1.firebaseio.com",
@@ -24,42 +24,44 @@ var config = {
 };
 firebase.initializeApp(config);
 
-var database = firebase.database();
+const database = firebase.database();
 
-var data;
+let data;
 function getdata(){
-  var db = firebase.database().ref('/climate');
-
-db.orderByChild('published_at').startAt(Date.now()).on('child_added', function(snapshot) {
-  console.log('Sendor Data', snapshot.val());
-  data = snapshot.val().data;
-});
-
-
-
+  const db = firebase.database().ref('/climate');
+  db.orderByChild('published_at').startAt(Date.now()).on('child_added', function(snapshot) {
+    console.log('Sendor Data', snapshot.val());
+    data = snapshot.val().data;
+  });
   // db.on('value',function(snapshot){
   //   var test = snapshot.val();
   //   console.log(test);
   // });
-} 
+}
 
-
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jade')
-app.use(express.logger('dev'))
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.logger('dev'));
 app.use(stylus.middleware(
-  { src: __dirname + '/public'
-  , compile: compile
+  {
+    src: __dirname + '/public',
+    compile: compile
   }
-))
+));
+
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (req, res) {
   res.render('index',
-  { title : 'Home',
-    data: require("os").hostname() }
+    {
+      title : 'Home',
+      data: require("os").hostname()
+    }
   )
-})
+});
 
-app.listen(3000)
-getdata()
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), () => {
+  console.log('Assister on port ' + app.get('port'));
+});
+getdata();
