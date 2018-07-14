5b7cd6f7501ee7b37992d5884d571c1c07eae217
Oscar Shaw <oscar.shaw@yahoo.com>
2018-07-14 14:33:33 -0700

    models.js: Upload

---
+++ documentation/models.js
@@ -0,0 +1,15 @@
+intent: {
+  name,
+  articles: [
+    article: {
+      name,
+      linkurl,
+      snippet,
+      upvotes,
+      doctors: [{
+        name,
+        education,
+      }]
+    }
+  ]
+}
--- server.js
+++ server.js
@@ -1,25 +1,15 @@
 const bodyParser = require('body-parser');
 const express = require('express');
-// const dialogflow = require('./server/dialogflow.js');
+const dialogflow = require('./server/dialogflow.js');
 const firebase = require('./server/firebase.js');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-// https://oshaw-vacspider-backend.herokuapp.com/api/diseases?name=varicella
-app.get('/api/diseases', function (request, response) {
-  // console.log(JSON.stringify(request, null, 2));
+app.get('/api/diseases', (request, response) => {
+  // dialogflow('Hello', () => {});
   response.send(`Received ${request.query.name}`);
-  // switch (request.query.name) {
-  //   case 'varicella': {
-  //     response.send('Received the message');
-  //     break;
-  //   }
-  // }
-  // firebase.push('doctors', {key: 'value'}, (error, firebaseResponse) => {
-  //   response.send(firebaseResponse);
-  // });
 });
 
 app.set('port', (process.env.PORT || 5000));
