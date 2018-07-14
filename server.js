5e714e9e18c829cdc08deaf49ba813c9d0d58a67
SangBin Cho <sangbin.cho@MacBookPro-LT390.local>
2018-07-14 15:32:47 -0700

    added search query endpoint

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const dialogflow = require('./server/dialogflow.js');
 const firebase = require('./server/firebase.js');
 const app = express();
+const search = require('./controller/customSearch').search;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -10,6 +11,11 @@ app.use(bodyParser.json());
 app.get('/api/diseases', (request, response) => {
   // dialogflow('Hello', () => {});
   response.send(`Received ${request.query.name}`);
+});
+
+app.get('/api/search', (request, response) => {
+  const searchQuery = request.query.searchQuery
+  search(request, response, searchQuery);
 });
 
 app.set('port', (process.env.PORT || 5000));
