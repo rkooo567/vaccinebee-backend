67bad095dd6a19b07e3a42d7d58f9916cf159e62
Oscar Shaw <oscar.shaw@yahoo.com>
2018-07-14 19:19:42 -0700

    server.js: Log body

--- server.js
+++ server.js
@@ -5,13 +5,8 @@ const firebase = require('./server/firebase.js');
 const app = express();
 const search = require('./controller/customSearch');
 const {WebhookClient, Card, Suggestion} = require('dialogflow-fulfillment');
-
-const {
-  dialogflow,
-  Image,
-} = require('actions-on-google')
+const {dialogflow, Image} = require('actions-on-google')
  
-
 app.use('/public', express.static(process.cwd() + '/public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -30,37 +25,13 @@ app.post('/api/dialogflow', (request, response) => {
   const agent = new WebhookClient({ request, response });
   console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
   console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
-
-  function welcome(agent) {
+const searchByAge = (agent) => {
+    console.log(JSON.stringify(request.body, null, 2));
     agent.add(`i am an idiot.`);
   }
-
-
-
-  // // Uncomment and edit to make your own intent handler
-  // // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
-  // // below to get this funciton to be run when a Dialogflow intent is matched
-  // function yourFunctionHandler(agent) {
-  //   agent.add(`This message is from Dialogflow's Cloud Functions for Firebase editor!`);
-  //   agent.add(new Card({
-  //       title: `Title: this is a card title`,
-  //       imageUrl: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
-  //       text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
-  //       buttonText: 'This is a button',
-  //       buttonUrl: 'https://assistant.google.com/'
-  //     })
-  //   );
-  //   agent.add(new Suggestion(`Quick Reply`));
-  //   agent.add(new Suggestion(`Suggestion`));
-  //   agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
-  // }
-
-  // Run the proper function handler based on the matched Dialogflow intent name
-  let intentMap = new Map();
-  intentMap.set('searchByAge', welcome);
-  // intentMap.set('your intent name here', yourFunctionHandler);
+  const intentMap = new Map();
+  intentMap.set('searchByAge', searchByAge);
   agent.handleRequest(intentMap);
-
 });
 
 app.get('/api/searchAdd', (request, response) => {
